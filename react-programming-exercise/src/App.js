f5f19bb2e8ae74ad095ff2cffbfa1dcc11a2ed4e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-02-26 21:15:21 +0200

    Convert data to JSON

--- react-programming-exercise/src/App.js
+++ react-programming-exercise/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+let  Profile = require('./data/sensor_readings.json');
+// import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+debugger
+
+// axios.get('./data/sensor_readings.json')
+//   .then(res => console.log(res));
+
+class App extends Component {
+
+  state = {
+    data: 'test'
+  }
+
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
 }
+
+// App.propTypes = {
+
+// };
 
 export default App;
